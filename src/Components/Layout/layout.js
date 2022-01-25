ef2b6ac23c47717580172dffe2f354f96529185a
rishav4101 <rajkumarrishav4101@gmail.com>
2022-01-25 14:11:35 +0530

    update

--- src/Components/Layout/layout.js
+++ src/Components/Layout/layout.js
@@ -8,9 +8,9 @@ function Layout(props) {
         <div className="content">{props.children}</div>
         <footer>
           <div className="logos">
-          <a href="https://www.facebook.com/debatingsociety3103.nitd/" target="_blank" ><img alt="." src="Images/fb.png" className="footIconSmall" /></a>
-          <a href="https://www.debsocnitdgp.in/" target="_blank" ><img alt="." src="Images/ds.png" className="footIcon" /></a>
-          <a href="https://instagram.com/debsocnitd?utm_medium=copy_link" target="_blank" ><img alt="." src="Images/ins.png" className="footIconSmall" /></a>
+          <a href="https://www.facebook.com/debatingsociety3103.nitd/" target="_blank" rel="noreferrer" ><img alt="." src="Images/fb.png" className="footIconSmall" /></a>
+          <a href="https://www.debsocnitdgp.in/" target="_blank" rel="noreferrer" ><img alt="." src="Images/ds.png" className="footIcon" /></a>
+          <a href="https://instagram.com/debsocnitd?utm_medium=copy_link" target="_blank" rel="noreferrer" ><img alt="." src="Images/ins.png" className="footIconSmall" /></a>
           </div>
           <div className="footText">The Debating Society, NIT Durgapur</div>
         </footer>
