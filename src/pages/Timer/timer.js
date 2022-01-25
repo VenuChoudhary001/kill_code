ea0f08dc3824c680820ac15cfa365e86be23851f
Shru-Singh <gshrutisingh@gmail.com>
2022-01-25 23:34:00 +0530

    timer updated

--- src/pages/Timer/timer.js
+++ src/pages/Timer/timer.js
@@ -1,16 +1,51 @@
 import "./timer.scss";
+import React, { useEffect, useState } from "react";
 
 function Timer() {
+  const [clock, setClock] = useState("00:00:00");
+  useEffect(() => {
+    const getCountdown = async () => {
+      var current_date = new Date().getTime();
+      let tf = new Date(
+        "Tue Feb 01 2022 18:00:00 GMT+0530 (India Standard Time)"
+      );
+      var target_date = tf.getTime();
+      var days, hours, minutes, seconds;
+
+      // find the amount of "seconds" between now and target
+      var seconds_left = (target_date - current_date) / 1000;
+
+      days = pad(parseInt(seconds_left / 86400));
+      seconds_left = seconds_left % 86400;
+
+      hours = pad(parseInt(seconds_left / 3600));
+      seconds_left = seconds_left % 3600;
+
+      minutes = pad(parseInt(seconds_left / 60));
+      seconds = pad(parseInt(seconds_left % 60));
+
+      // format countdown string + set tag value
+      const html = [days, hours, +minutes, +seconds];
+      setClock(html);
+    };
+
+    function pad(n) {
+      return (n < 10 ? "0" : "") + n;
+    }
+
+    getCountdown();
+  });
+
   return (
     <>
       <div className="timer">
         <div className="card">
           <div className="cardHead">
             <div className="labelsTime">
-              <span>04</span>
-              <span>45</span>
-              <span>40</span>
-              <span>05</span>
+              <span>{clock[0]}</span>
+              <span>{clock[1]}</span>
+              <span>{clock[2]}</span>
+              <span>{clock[3]}</span>
             </div>
             <div className="labels">
               <span>Days</span>
