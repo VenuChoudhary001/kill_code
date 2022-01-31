2c9dabe0275ee228be0b421beb89c4b268ad0e4e
Venu <venu.choudhary2000@gmail.com>
2022-01-31 23:31:26 +0530

    map update 1.1

--- src/pages/Locations/index.js
+++ src/pages/Locations/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../../Components/Navbar";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
