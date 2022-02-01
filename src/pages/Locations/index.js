b5bb2add93abef6aa8c68f381c33852d85968b93
Venu <venu.choudhary2000@gmail.com>
2022-02-01 18:02:53 +0530

    map

--- src/pages/Locations/index.js
+++ src/pages/Locations/index.js
@@ -6,81 +6,81 @@ import STORE from "../../Context/store";
 const LOCATIONS = () => {
   const navigate = useNavigate();
   const {setActive}=useContext(STORE);
-  const data = [
-    {
-      name: "Wakanda",
-      Latitude: 61.05654,
-      Longitude: -64.75786,
-    },
-    {
-      name: "Sokovia",
-      Latitude: 7.72751,
-      Longitude: 13.00909,
-    },
-    {
-      name: "Asgard",
-      Latitude: 65.65881,
-      Longitude: 25.28571,
-    },
-    {
-      name: "Queens",
-      Latitude: 51.80406,
-      Longitude: -73.21521,
-    },
-    {
-      name: "Kamar-Taj",
-      Latitude: 45.301,
-      Longitude: 21.4536,
-    },
-    {
-      name: "Svartalfheim",
-      Latitude: 11.7513,
-      Longitude: 96.0991,
-    },
-    {
-      name: "Tonesburg",
-      Latitude: 8.28037,
-      Longitude: -144.33455,
-    },
-    {
-      name: "Alfheim",
-      Latitude: 80.62089,
-      Longitude: 124.04041,
-    },
-    {
-      name: "Morag",
-      Latitude: -3.92118,
-      Longitude: 103.90839,
-    },
-    {
-      name: "Ta Lao",
-      Latitude: -12.30682,
-      Longitude: -83.82595,
-    },
-    {
-      name: "Muspelheim",
-      Latitude: 67.65881,
-      Longitude: 60.28571,
-    },
-    {
-      name: "Titan",
-      Latitude: 11.58501,
-      Longitude: -79.12818,
-    },
-    {
-      Latitude: 41.61829,
-       Longitude: -111.94126,
-      name: "Afghanistan"
-    },{
-      Latitude: 52.75235, Longitude: -29.57541,
-      name:"Xandar"
-    },
-    {
-      Latitude: 33.84501, Longitude: -52.86823,name:"Citadel"
-    },{
-      Latitude: 22.10288, Longitude: 63.09014,name:"Sanctum Sanctorum"
-    }
-  ];
+  // const data = [
+  //   {
+  //     name: "Wakanda",
+  //     Latitude: 61.05654,
+  //     Longitude: -64.75786,
+  //   },
+  //   {
+  //     name: "Sokovia",
+  //     Latitude: 7.72751,
+  //     Longitude: 13.00909,
+  //   },
+  //   {
+  //     name: "Asgard",
+  //     Latitude: 65.65881,
+  //     Longitude: 25.28571,
+  //   },
+  //   {
+  //     name: "Queens",
+  //     Latitude: 51.80406,
+  //     Longitude: -73.21521,
+  //   },
+  //   {
+  //     name: "Kamar-Taj",
+  //     Latitude: 45.301,
+  //     Longitude: 21.4536,
+  //   },
+  //   {
+  //     name: "Svartalfheim",
+  //     Latitude: 11.7513,
+  //     Longitude: 96.0991,
+  //   },
+  //   {
+  //     name: "Tonesburg",
+  //     Latitude: 8.28037,
+  //     Longitude: -144.33455,
+  //   },
+  //   {
+  //     name: "Alfheim",
+  //     Latitude: 80.62089,
+  //     Longitude: 124.04041,
+  //   },
+  //   {
+  //     name: "Morag",
+  //     Latitude: -3.92118,
+  //     Longitude: 103.90839,
+  //   },
+  //   {
+  //     name: "Ta Lao",
+  //     Latitude: -12.30682,
+  //     Longitude: -83.82595,
+  //   },
+  //   {
+  //     name: "Muspelheim",
+  //     Latitude: 67.65881,
+  //     Longitude: 60.28571,
+  //   },
+  //   {
+  //     name: "Titan",
+  //     Latitude: 11.58501,
+  //     Longitude: -79.12818,
+  //   },
+  //   {
+  //     Latitude: 41.61829,
+  //      Longitude: -111.94126,
+  //     name: "Afghanistan"
+  //   },{
+  //     Latitude: 52.75235, Longitude: -29.57541,
+  //     name:"Xandar"
+  //   },
+  //   {
+  //     Latitude: 33.84501, Longitude: -52.86823,name:"Citadel"
+  //   },{
+  //     Latitude: 22.10288, Longitude: 63.09014,name:"Sanctum Sanctorum"
+  //   }
+  // ];
   /* eslint-disable */
   useEffect(() => {
     if (
